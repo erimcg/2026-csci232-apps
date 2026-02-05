a85998b978b709a8210f40b637350ab012ee0652
Eric <eric@Erics-MacBook-Pro-3.local>
2026-02-04 23:45:27 -0500

    demo0

---
+++ demo9-provide-inject/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import Square from './components/Square.vue';
+  import { ref, provide } from 'vue'
+
+  const shape = ref('circle')
+  const count = ref(67)
+
+  provide('shape', shape)
+  provide('count', { count, toggleShape })
+
+  function toggleShape() {
+    shape.value = shape.value === 'circle' ? 'triangle' : 'circle'
+  }
+
+</script>
+
+<template>
+  <Square />
+  <button @click="toggleShape">&Delta; Shape</button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ demo9-provide-inject/src/components/Square.vue
@@ -0,0 +1,32 @@
+<script setup>
+    import Circle from './Circle.vue';
+    import Triangle from './Triangle.vue';
+    import { inject } from 'vue'
+
+    const shape = inject('shape')
+</script>
+
+<template>
+  <div class="square">
+    <template v-if="shape === 'circle'">
+        <Circle />
+    </template>
+    <template v-else>
+        <Triangle />
+    </template>
+    
+  </div>
+</template>
+
+<style scoped>
+.square {
+    width: 200px;
+    height: 200px;
+
+    background-color: gainsboro;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
